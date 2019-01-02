83666537d4baa7c0e227bd2f07815ad2562d8c92
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-02 00:11:26 -0500

    lesson 7

--- start/app.js
+++ start/app.js
@@ -1,6 +1,12 @@
 new Vue({
     el: "#app",
     data: {
-        title: "Hello VUE!"
+        title: "Hello VUE!",
+        name: "mehdi"
+    },
+    methods: {
+        greet(time) {
+            return `wow this is very cool! lets keep going ${this.name}! now is ${time} better get some sleep.`
+        }
     }
 })
